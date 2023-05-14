037a5964a400aeeb5b2c032eb76b180b46a5e7e7
ddPn08 <pyn.g.1125@gmail.com>
2023-05-14 14:03:28 +0900

    small fix

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as requirements_file:
-    install_requirements = requirements_file.read().splitlines()
-
 setup(
     name="hf_utils",
     version="0.0.1",
     description="huggingface_hub utils",
     author="ddPn08",
     packages=find_packages(),
-    install_requires=install_requirements,
+    install_requires=[
+        "huggingface_hub",
+        "fire",
+    ],
     entry_points={
         "console_scripts": [
             "hf_utils=hf_utils.core:main",
